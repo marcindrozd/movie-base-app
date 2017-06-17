8a970a992448d53ee1ff02c5bf88b6a71bb7290f
Marcin Drozd <marcindrozd@me.com>
2017-06-17 17:04:33 +0200

    Add jwt authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate
+
   def index; end
 end
--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module ReactApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.autoload_paths << Rails.root.join("lib")
   end
 end
